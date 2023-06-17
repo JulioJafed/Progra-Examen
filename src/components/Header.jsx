bd0ce83bf492858144737471995725533f534e87
JulioJafed <128091133+JulioJafed@users.noreply.github.com>
2023-06-17 17:16:38 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+//import React from 'react'
+import "../components/header.css"
+const Header = () => {
+  return (
+    <>
+        <p className="Titulo">Julio Cabrera </p>
+        
+    </>
+  )
+}
+
+export default Header
